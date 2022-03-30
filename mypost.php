7e7d14c625b5ebf7aedd39cd324ec199dff1c059
iamlinjai <21035467d@connect.polyu.hk>
2022-03-30 17:22:18 +0800

    update mypost.php

--- mypost.php
+++ mypost.php
@@ -16,8 +16,82 @@ include 'connection.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Post</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
+<body>
+    <div class="content">
+        <label id="header">Your Product</label>
+
+        <br><br>
+
+        <?php
+        $sql = "SELECT * FROM item WHERE userid ='" . $_SESSION['userid'] . "' AND item_status = 'bidding' ";
+
+        $result = mysqli_query($connect, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+
+                <a href="login.php"><button type="button" id="mypostbtn">
+                        <table id="selling-display">
+
+                            <tr>
+                                <td><img src="upload/<?php echo $row['item_img']; ?>" alt="<?php echo $row['item_title']; ?>" id="smallimg"></td>
+                            </tr>
+
+                            <tr>
+                                <td>house</td>
+                            </tr>
+
+                            <tr>
+                                <td id="display-price">$ <?php echo $row['price']; ?></td>
+                            </tr>
+
+                            <tr>
+                                <td id="selling">On sales</td>
+                            </tr>
+
+                        </table>
+                    </button></a>
+
+                <a href="login.php"><button type="button" id="mypostbtn">
+                        <table id="selling-display">
+
+                            <tr>
+                                <td><img src="upload/building.jpg" id="smallimg"></td>
+                            </tr>
+
+                            <tr>
+                                <td><?php echo $row['item_title']; ?></td>
+                            </tr>
+
+                            <tr>
+                                <td>$ 1000</td>
+                            </tr>
+
+                            <tr>
+                                <td id="sold">Sold</td>
+                            </tr>
+
+                        </table>
+                    </button></a>
+
+        <?php
+            }
+        }
+        ?>
+    </div>
+</body>
+
+</html>
+
+
+
+<!---------------------------------------------------------------Backup Code------------------------------------------------------------------------------->
+
+<!--
 <body>
     <br>
     <label id="header">My Posts</label>
@@ -55,7 +129,7 @@ include 'connection.php';
 
                                 <?php
 
-                                  
+
                                 if (isset($_POST['cancelbid'])) {
 
                                     $curitemid = $row['itemid'];
@@ -79,3 +153,4 @@ include 'connection.php';
 </body>
 
 </html>
+    -->
